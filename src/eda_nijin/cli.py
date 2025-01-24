1637eb32fc53d5274295e9ba2314dd3488abb3af
heejin131 <journeyjean131@gmail.com>
2025-01-24 16:14:54 +0900

    다양한chart

--- src/eda_nijin/cli.py
+++ src/eda_nijin/cli.py
@@ -53,7 +53,12 @@ def print_group_by_count(keyword:str, asc:bool=False, rcnt:int=12, keyword_sum:b
     #print(df.to_string(index=False))
     print(t)
     print(f"총 합계:{len(df)}")
-  
+
+    import termplotlib as tpl
+    fig = tpl.figure()
+    fig.barh(df['count'], df['president'], force_ascii=True)
+    fig.show()
+
 def entry_point():
     typer.run(print_group_by_count)
 
